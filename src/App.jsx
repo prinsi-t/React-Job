d9e21dac995620ba0ab3eb1d2843e951805f3ce3
prinsi-t <tprincy56@gmail.com>
2025-11-18 21:18:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='font-mono text-blue-500 text-3xl text-center mt-10'>
+      App works!
+    </div>
+  )
+}
+
+export default App
